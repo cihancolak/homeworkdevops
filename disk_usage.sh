f862146553cdcfb6bf63f1ed85c473b1f3298a26
Cihan <47649049+cihancolak@users.noreply.github.com>
2024-05-11 00:53:41 +0300

    Update disk_usage.sh

--- disk_usage.sh
+++ disk_usage.sh
@@ -4,7 +4,7 @@
 trap 'tput cnorm' EXIT
 tput setaf 7  # White text colour
 
-# Controlled disk usage
+# Control disk usage
 disk_usage=$(df -h /home | awk 'NR==2 {print $5}' | sed 's/%//')
 if [ $disk_usage -gt 80 ]; then
   tput setaf 1  # Red text colour
@@ -14,15 +14,25 @@ fi
 
 # Backup Logs
 log_dir="/var/log"
-backup_dir="/backup/logs"
+backup_dir="./backup/logs"
 if [ ! -d "$backup_dir" ]; then
   mkdir -p "$backup_dir"
 fi
 cp "$log_dir"/*.log "$backup_dir"
 echo "Logs '$backup_dir' backup saved."
 
-# Create system health report.
-report_file="/home/admin/system_health_$(date +%F).txt"
+# if report folder not created and then create new folder
+system_health_dir=."/home/admin"
+if [ ! -d "$system_health_dir" ]; then
+  mkdir -p "$system_health_dir"
+fi
+
+# Create system health report file name.
+report_file="home/admin/system_health_$(date +%F).txt"
+
+# touch and put system health report to report_file
+touch "$report_file"
+
 {
   echo "System Health Report"
   echo "------------------"
